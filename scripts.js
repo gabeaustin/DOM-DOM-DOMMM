1b76f3573b641b9276f67c71fdbebf6032060141
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-01-28 02:04:37 -0600

    worked on adding id to divs-WIP - not finished

--- scripts.js
+++ scripts.js
@@ -1,17 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let button = document.createElement("button");
     let buttonText = document.createTextNode("Add Square");
     button.appendChild(buttonText);
     document.body.appendChild(button);
 
-    button.addEventListener("click", function() {
+    button.addEventListener("click", function () {
         let div = document.createElement("div");
         document.body.appendChild(div);
         div.style.border = "1px solid black";
-        div.className = "flex-container";
+        div.className = "flex-container newDiv";
         div.style.display = "flex";
         div.style.flexDirection = "row";
+        div.style.alignContent = "flex-start";
+        div.style.flexWrap = "wrap";
 
-        
+
+
+
     })
 })
